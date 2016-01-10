cf1a56f4c4c1760ed29be5d3660cb8eb9bf0abe2
Bhuvnesh Chaudhary <bchaudhary@pivotal.io>
2016-01-10 01:18:42 -0800

    updated cardinality

--- install_ambari_cluster.sh
+++ install_ambari_cluster.sh
@@ -10,7 +10,7 @@ set -e
 # To test your ambari development build, update XXXXX_TARBALL_DOWNLOAD_LINK
 # link to the path of the tarball from where it can be downloaded
 ###########################################################################################
-AMBARI_TARBALL_DOWNLOAD_LINK="https://jenkins.eng.pivotal.io/jenkins/view/AMBR-OSS-BUILD/job/AMBR-OSS-BUILD-trunk/lastSuccessfulBuild/artifact/target/AMBARI-trunk-PHD-latest.tar.gz"
+AMBARI_TARBALL_DOWNLOAD_LINK="https://jenkins.eng.pivotal.io/jenkins/view/AMBR-OSS-BUILD/job/AMBR-OSS-BUILD-BRANCH-1/lastSuccessfulBuild/artifact/target/AMBARI-Add-Standby-Wizard-PHD-latest.tar.gz"
 PHD_TARBALL_DOWNLOAD_LINK="http://internal-dist-elb-877805753.us-west-2.elb.amazonaws.com/dist/hortonworks/certified/PHD-3.3.2.0-2950-centos6.tar.gz" 
 PHD_UTILS_TARBALL_DOWNLOAD_LINK="http://internal-dist-elb-877805753.us-west-2.elb.amazonaws.com/dist/hortonworks/certified/PHD-UTILS-1.1.0.20-centos6.tar.gz"
 HDB_TARBALL_DOWNLOAD_LINK="http://internal-dist-elb-877805753.us-west-2.elb.amazonaws.com/dist/HAWQ/stable/pivotal-hdb-latest-stable.tar.gz"
@@ -162,8 +162,8 @@ fi
 setup_tars
 setup_vagrant ${NODES}
 setup_ambari_server
-if [ ${user_nodes_input} -eq "2" ]; then
-  bootstrap
-fi
-create_cluster ${BLUEPRINT_NAME}.json ${HOSTMAPPING_FILENAME}.json
+#if [ ${user_nodes_input} -eq "2" ]; then
+#  bootstrap
+#fi
+#create_cluster ${BLUEPRINT_NAME}.json ${HOSTMAPPING_FILENAME}.json
 echo "Please verify cluster creation progress at: http://c6401.ambari.apache.org"
